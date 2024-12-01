432af5ddb58badbc015f7f92bffdac0152927f9b
Le Duong <141017307+lenduong@users.noreply.github.com>
2024-12-01 10:23:59 -0800

    Update server.py

--- final_project/http/server.py
+++ final_project/http/server.py
@@ -16,24 +16,25 @@ app = Flask('RaspberryPi Mailbox Server')
 
 @app.route('/send_image', methods=['POST'])
 def post_image_callback():
-        # Check if an image is in the request
-        if 'image' not in request.files:
-                return jsonify({"error": "No image file in request"}), 400
+        # # Check if an image is in the request
+        # if 'image' not in request.files:
+        #         return jsonify({"error": "No image file in request"}), 400
+        # Ensure the content type is JPEG
+        if request.content_type != 'image/jpeg':
+                return jsonify({"error": "Invalid content type. Only JPEG is supported."}), 400
+        # # Retrieve the image
+        # image_file = request.files['image']
         
-        # Retrieve the image
-        image_file = request.files['image']
-        
-        # Save the image locally (optional)
-        raw_path = f'uploads/{image_file.filename}'
-        save_path = f'uploads/{image_file.filename}.jpg'
-        image_file.save(raw_path)
+        # # Save the image locally (optional)
+        # raw_path = f'uploads/{image_file.filename}'
+        # save_path = f'uploads/{image_file.filename}.jpg'
+        # image_file.save(raw_path)
 
-        with Image.open(raw_path) as img:
-                # Convert to RGB mode (JPEG doesn't support transparency)
-                img = img.convert("RGB")
-                
-                # Save the image as JPEG
-                img.save(save_path, "JPEG")
+        # Save the image data
+        image_data = request.data
+        save_path = f'uploads/{image_file.filename}.jpg'
+        with open(save_path, "wb") as f:
+                f.write(image_data)
     
         return jsonify({"message": f"Image {image_file.filename} received and saved at {save_path}"}), 200
 
