7840dfa4767d0069904529e830160836c1b5cc8c
Le Duong <141017307+lenduong@users.noreply.github.com>
2024-12-02 12:08:31 -0800

    Update deploy.py

--- final_project/deploy.py
+++ final_project/deploy.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
   # Find the hand number image the model should read/predict
   # image_path = 'g_1.png'
-  image_path = '/mnt/c/Users/leduo/Desktop/EE250-Collab/final_project/capturing_single_image/g_0.png'
+  image_path = '/mnt/c/Users/leduo/Desktop/EE250-Collab/final_project/capturing_single_image/g_5.png'
   # if len(sys.argv) > 0:
   #   image_path = sys.argv[0]
   
