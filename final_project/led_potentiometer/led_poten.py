6d4009f4e19a1e82db2d0e6f046db2d578c782db
Le Duong <141017307+lenduong@users.noreply.github.com>
2024-12-02 22:11:45 -0800

    Create led_poten.py

---
+++ final_project/led_potentiometer/led_poten.py
@@ -0,0 +1,65 @@
+import time
+import RPi.GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+#using physical pin 11 to blink Red LED, 15 to blink Yellow LED
+GPIO.setmode(GPIO.BOARD)
+red_led = [11]
+yellow_led = [15]
+GPIO.setup(red_led, GPIO.OUT)
+GPIO.setup(yellow_led, GPIO.OUT)
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+
+while True:  
+  
+  GPIO.output(red_led, GPIO.HIGH)
+  print("turning on Red LED")
+  print("Potentiometer Channel 0: ", mcp.read_adc(0))
+  print("Potentiometer Channel 1: ", mcp.read_adc(1))
+  time.sleep(1)
+  GPIO.output(red_led, GPIO.LOW)
+  GPIO.output(yellow_led, GPIO.HIGH)
+  print("turning on Yellow LED")
+  print("Potentiometer Channel 0: ", mcp.read_adc(0))
+  print("Potentiometer Channel 1: ", mcp.read_adc(1))
+  time.sleep(1)
+  GPIO.output(yellow_led, GPIO.LOW)
+
+  
+  # #Following commands control the state of the output
+  # for i in range (0,5):
+  #   GPIO.output(chan_list, GPIO.HIGH)
+  #   time.sleep(0.5)
+  #   GPIO.output(chan_list, GPIO.LOW)
+  #   time.sleep(0.5)
+  # for i in range (0,50):
+  #   time.sleep(0.1)
+  #   light_reading = mcp.read_adc(0)
+  #   if light_reading >lux_treshold:
+  #     print("(BRIGHT) Light sensor reading: ", mcp.read_adc(0))
+  #   else :
+  #     print("(DARK) Light sensor reading: ", mcp.read_adc(0))
+
+  # for i in range (0,4):
+  #   GPIO.output(chan_list, GPIO.HIGH)
+  #   time.sleep(0.2)
+  #   GPIO.output(chan_list, GPIO.LOW)
+  #   time.sleep(0.2)
+
+  # for i in range (0,50):
+  #   time.sleep(0.1)
+  #   sound_reading = mcp.read_adc(1)
+  #   if sound_reading >sound_treshold:
+  #     GPIO.output(chan_list, GPIO.HIGH)
+  #   else :
+  #     GPIO.output(chan_list, GPIO.LOW)
+
+  # # get reading from adc 
+  # print("Light sensor reading: ", mcp.read_adc(0))
+  # print("Sound sensor reading: ", mcp.read_adc(1))
