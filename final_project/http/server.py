95e5c07776105b7bd93234587b4198d45c4f28ac
grish-ma <144985869+grish-ma@users.noreply.github.com>
2024-12-03 15:23:27 -0800

    Update LED_command

--- final_project/http/server.py
+++ final_project/http/server.py
@@ -31,13 +31,16 @@ from threading import Lock
 # ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
 # import pickle
+LED_command # global variable
 
 app = Flask('RaspberryPi Mailbox Server')
 loaded_model = keras.models.load_model('handNums_model-1104.h5')
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    if LED_command is None:
+        return "No value detected yet."
+    return LED_command
 
 @app.route('/send_image', methods=['POST'])
 def post_image_callback():
@@ -61,7 +64,7 @@ def post_image_callback():
     with open(save_path, "wb") as f:
             f.write(image_data)
         
-    LED_command = deploy()
+    global LED_command = deploy()
     
     return jsonify({"message1": f"Image received and saved as {save_path}", "message2":LED_command}), 200
 
@@ -88,14 +91,13 @@ def deploy():
     print(predict_value)
 
     # Taking model output and converting it to be ON or OFF for LEDs
-    # if predict_value % 2 == 0:
-    #     print("LED ON")
-    #     return True
-    # else:
-    #     print("LED OFF")
-    #     return False
+    # Even values produce LED ON
+    if predict_value % 2 == 0:
+        print("LED ON")
+        return True
+    else: # Odd value produce LED OFF
+        print("LED OFF")
     return False
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
