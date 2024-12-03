ac6537e1111c547c02110735b78d1591f47cbb69
Le Duong <141017307+lenduong@users.noreply.github.com>
2024-12-03 12:24:10 -0800

    Update client.py

--- final_project/http/client.py
+++ final_project/http/client.py
@@ -1,46 +1,74 @@
 import requests
 import cv2
+import time
+import RPi.GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
 
 if __name__ == '__main__':
-        url = "http://192.168.91.71:8080/send_image"
-        # url = "http://192.168.91.71:5000/send_image"
-        # url = "http://172.26.240.1:5000/send_image"
-        # url = "http://172.26.242.207:8080/send_image"
+        # ------------------------Set up for LED and Potentiometer---------------------------
+        #using physical pin 11 to blink Red LED, 15 to blink Yellow LED
+        GPIO.setmode(GPIO.BOARD)
+        red_led = [11]
+        yellow_led = [15]
+        GPIO.setup(red_led, GPIO.OUT)
+        GPIO.setup(yellow_led, GPIO.OUT)
         
+        # Hardware SPI configuration:
+        SPI_PORT   = 0
+        SPI_DEVICE = 0
+        mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+        
+        flag = True # True = Red is on, False = Yellow is on
+        led = [11]
+        # -------------------------Record and send image from RPi to Server ----------------------
         # Start recording
         cam = cv2.VideoCapture(0)
-        
-        #while True:
+    
+        # Caputure the imgage
         ret, image = cam.read()
-        #cv2.imshow('Imagetest',image)
-        #k = cv2.waitKey(1)
-        #if k != -1:
-        #break
-        
-        #image_path = r'~/ee250_fp_grish_le/testing/testimage.jpg'
-        #image = cv2.imread(image_path)
-        
-        # cv2.imwrite('./testing/testimage2.jpg', image)
         cam.release()
-        # ----------------------------------
-        # # Open the image file in binary mode
-        # # with open("your_image.jpg", "rb") as image_file:
-        # # files = {"image": image_file}
-        # files = {"image": image}
-        
-        # # Send the POST request
-        # response = requests.post(url, files=files)
-
+    
+        # Turn the image into jpg file
         _, buffer = cv2.imencode('.jpg', image)
-
+    
         # Send the image via HTTP POST
         headers = {"Content-Type": "image/jpeg"}  # Indicate JPEG format
         response = requests.post(url, data=buffer.tobytes(), headers=headers)
-        message = response.json() # Parse the JSON file
+        # ----------------------Process response from server---------------------------
+        # Parse the JSON file
+        # Message1 : image upload confirmation message
+        # Message2 : command to turn on or off light, ouput by ML model
+        message = response.json()
+    
         # Check the response status code
         if response.status_code == 200:
             print("Image uploaded successfully")
             print(message["message1"])
+            # If message2 = True turn on light, else turn off
+            if (mcp.read_adc(0) > 530) and (flag == False) and message["message2"]:
+                # If potentiometer is turned to upper half, turn on Red LED
+                GPIO.output(led, GPIO.LOW)
+                led = [11]
+                flag = True
+                GPIO.output(led, GPIO.HIGH)
+                print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+                print("Turning on Red LED")
+                print("Potentiometer Channel 0: ", mcp.read_adc(0))
+                print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+            elif (mcp.read_adc(0) <= 500) and (flag == True) and message["message2"]: 
+                # If potentiometer is turned to lower half, turn on Yellow LED
+                GPIO.output(led, GPIO.LOW)
+                led = [15]
+                flag = False
+                GPIO.output(led, GPIO.HIGH)
+                print("-------------------------------------------")
+                print("Turning on Yellow LED")
+                print("Potentiometer Channel 0: ", mcp.read_adc(0))
+                print("-------------------------------------------")
+            elif not message["message2"]:
+                GPIO.output(led, GPIO.LOW)
         else:
             print("Error uploading image:", response.status_code)
 
