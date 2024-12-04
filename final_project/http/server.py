d2a61429e161238089acdf57ec5df62f482ffd55
grish-ma <144985869+grish-ma@users.noreply.github.com>
2024-12-03 18:09:33 -0800

    Update server.py

--- final_project/http/server.py
+++ final_project/http/server.py
@@ -39,13 +39,12 @@ loaded_model = keras.models.load_model('handNums_model-1104.h5')
 
 @app.route('/')
 def home():
-    # global LED_command
-    # # message = str(LED_command)
-    # # return message
-    # if LED_command == True:
-    #     return render_template('frontEnd-ON.html')
-    # return render_template('frontEnd-OFF.html')
-    return render_template('frontEnd-ON.html')
+    global LED_command
+    # message = str(LED_command)
+    # return message
+    if LED_command == True:
+        return render_template('frontEnd-ON.html')
+    return render_template('frontEnd-OFF.html')
     
 
 @app.route('/send_image', methods=['POST'])
