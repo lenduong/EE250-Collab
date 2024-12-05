79059052ac1199d3f1a3690852f35d82849dba31
Le Duong <141017307+lenduong@users.noreply.github.com>
2024-12-05 15:41:51 -0800

    Update server.py - clean up code

--- final_project/http/server.py
+++ final_project/http/server.py
@@ -1,80 +1,67 @@
+#----------- Imports for HTTP Flask Server -----------#
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import render_template
-from PIL import Image
+#-----------------------------------------------------#
 
-# Imports for loading the ML model #
+#--------- Imports for loading the ML model ----------#
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 from PIL import Image
 import matplotlib.pyplot as plt
+#-----------------------------------------------------#
 
+#-------------- Miscellaneous Imports ----------------#
+from PIL import Image
 import argparse
 import json
-#import mailboxManager
-
 from datetime import datetime
 from threading import Lock
+#-----------------------------------------------------#
 
-# ### Potentiometer Imports ### ### ### ### ### ### ### ### ###
-# import sys
-# import time
-# # By appending the folder of all the GrovePi libraries to the system path here,
-# # we are successfully `import grovepi`
-# sys.path.append('../../Software/Python/')
-# # This append is to support importing the LCD library.
-# sys.path.append('../../Software/Python/grove_rgb_lcd')
+# ----------------- GLobal Commands ------------------#
+app = Flask('RaspberryPi Mailbox Server') 
+loaded_model = keras.models.load_model('handNums_model-1104.h5') # load the trained ML model
+LED_command = False # global flag variable for LED
+#-----------------------------------------------------#
 
-# import grovepi
-# from grove_rgb_lcd import *
-# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-# import pickle
-LED_command = False # global variable
-
-app = Flask('RaspberryPi Mailbox Server')
-loaded_model = keras.models.load_model('handNums_model-1104.h5')
-
+# --------------------------- Code for HTTP Server ----------------------------#
+# Set up home page for web front end
+# Use http://localhost:8080/ to access front end
+# Change based on whether command is ON or OFF
 @app.route('/')
 def home():
     global LED_command
-    # message = str(LED_command)
-    # return message
     if LED_command == True:
         return render_template('frontEnd-ON.html')
     return render_template('frontEnd-OFF.html')
     
-
+# Custom callback function to receive image from RPi
 @app.route('/send_image', methods=['POST'])
 def post_image_callback():
-    # # Check if an image is in the request
-    # if 'image' not in request.files:
-    #         return jsonify({"error": "No image file in request"}), 400
     # Ensure the content type is JPEG
     if request.content_type != 'image/jpeg':
             return jsonify({"error": "Invalid content type. Only JPEG is supported."}), 400
-    # # Retrieve the image
-    # image_file = request.files['image']
-    
-    # # Save the image locally (optional)
-    # raw_path = f'uploads/{image_file.filename}'
-    # save_path = f'uploads/{image_file.filename}.jpg'
-    # image_file.save(raw_path)
 
-    # Save the image data
+    # Save the Image
     image_data = request.data
     save_path = f'uploads/test_image.jpg'
     with open(save_path, "wb") as f:
             f.write(image_data)
-        
+
+    # Call deploy() to predict the image
     global LED_command
     LED_command = deploy()
-    
+
+    # Return a response to client to confirm the request, along with the LED command
     return jsonify({"message1": f"Image received and saved as {save_path}", "message2":LED_command}), 200
+# -------------------------------------------------------------------------------#
 
 
+# --------------------------- Code for ML Prediction ----------------------------#
 # Define image pre-processing in a function
 def image_preprocessor(image_path):
   img = Image.open(image_path).convert('L')
@@ -104,11 +91,13 @@ def deploy():
     else: # Odd value produce LED OFF
         print("LED OFF")
     return False
+# -------------------------------------------------------------------------------#
 
+
+# --------------------------------- Main ----------------------------------------#
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080) # Begin the server
+# -------------------------------------------------------------------------------#
 
-    # # Load the trained model from the header file
-    # loaded_model = keras.models.load_model('handNums_model-1104.h5')
 
     
